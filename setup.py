6718406a3d6bf90827569c17fa0502912268a287
Lucretter <omar@aboukhait.com>
2025-09-13 19:08:54 +0200

    new file:   bs2pro 	deleted:    bs2pro_controller.py 	deleted:    settings.ini 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bs2pro-controller",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "ttkbootstrap",
+        "hidapi",
+    ],
+    entry_points={
+        "console_scripts": [
+            "bs2pro=bs2pro.main:main"
+        ]
+    },
+)
