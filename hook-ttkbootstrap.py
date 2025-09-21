8ebda03a139af04711859e9166cf5e252285372c
Lucretter <aboukhait.omar@gmail.com>
2025-09-21 13:15:19 +0200

    modified:   .github/workflows/release.yml 	modified:   hook-ttkbootstrap.py

--- hook-ttkbootstrap.py
+++ hook-ttkbootstrap.py
@@ -7,24 +7,32 @@ import os
 # Collect all ttkbootstrap data files
 datas = collect_data_files('ttkbootstrap')
 
-# Add Tcl library files if they exist
+# Add Tcl library files if they exist (with error handling)
 tcl_lib_paths = [
-    '/usr/lib/tcl8.6',
-    '/usr/lib/tk8.6',
-    '/usr/share/tcl8.6',
-    '/usr/share/tk8.6'
+    ('/usr/lib/tcl8.6', 'tcl8.6'),
+    ('/usr/lib/tk8.6', 'tk8.6'),
+    ('/usr/share/tcl8.6', 'tcl8.6'),
+    ('/usr/share/tk8.6', 'tk8.6')
 ]
 
-for path in tcl_lib_paths:
+for path, dest in tcl_lib_paths:
     if os.path.exists(path):
-        datas.append((path, 'tcl8.6' if 'tcl8.6' in path else 'tk8.6'))
+        try:
+            datas.append((path, dest))
+        except Exception as e:
+            # Silently continue if we can't add the path
+            pass
 
-# Add msgcat specifically
+# Add msgcat specifically if it exists
 msgcat_paths = [
-    '/usr/lib/tcl8.6/msgcat1.7.1',
-    '/usr/share/tcl8.6/msgcat1.7.1'
+    ('/usr/lib/tcl8.6/msgcat1.7.1', 'tcl8.6/msgcat1.7.1'),
+    ('/usr/share/tcl8.6/msgcat1.7.1', 'tcl8.6/msgcat1.7.1')
 ]
 
-for path in msgcat_paths:
+for path, dest in msgcat_paths:
     if os.path.exists(path):
-        datas.append((path, 'tcl8.6/msgcat1.7.1'))
+        try:
+            datas.append((path, dest))
+        except Exception as e:
+            # Silently continue if we can't add the path
+            pass
