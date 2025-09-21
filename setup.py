1c4a08d9f4d91e9f964c35515e25dddaeed7440d
Lucretter <aboukhait.omar@gmail.com>
2025-09-21 20:46:16 +0200

    Update version to 1.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bs2pro-controller",
-    version="1.0.0",
+    version="1.8.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
