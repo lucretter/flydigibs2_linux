b7fa8f3a0d1c40d70941b6d170f379641a6c3cf0
Lucretter <aboukhait.omar@gmail.com>
2025-09-23 01:37:04 +0200

    Update version to 2.4.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bs2pro-controller",
-    version='2.4.1',  # Fixed ConfigParser type error
+    version='2.4.2',  # Fixed HID device enumeration for newer hidapi versions
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
