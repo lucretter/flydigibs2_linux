8f9f695405ba340d285ba6bd069c68f971ff1b64
Lucretter <aboukhait.omar@gmail.com>
2025-09-23 01:32:16 +0200

    Update version to 2.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bs2pro-controller",
-    version='2.4.0',  # Updated for native PyQt6 GUI support
+    version='2.4.1',  # Fixed ConfigParser type error
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
