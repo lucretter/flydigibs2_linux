45ce54449ae46ad32c8cba59a3abc9b193af3d49
Lucretter <aboukhait.omar@gmail.com>
2025-09-24 00:07:23 +0200

    Bump version to 2.5.0 for major smart mode improvements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bs2pro-controller",
-    version="2.4.5",  # Added missing pyqtgraph dependency
+    version="2.5.0",  # Major smart mode improvements: graph mode, profiles, temperature sources
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
