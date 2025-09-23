7caa978e3e20c7a79dc683e88554dcf2adf19f73
Lucretter <aboukhait.omar@gmail.com>
2025-09-24 00:40:40 +0200

    Fix RPM numpy dependency for pyqtgraph
    - Add python3-numpy to RPM package dependencies
    - pyqtgraph requires numpy which was missing from RPM builds
    - Fixes ModuleNotFoundError: No module named 'numpy'
    
    Fixes RPM launch error after PyQt6 compatibility fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bs2pro-controller",
-    version="2.5.2",  # Fixed RPM PyQt6 bundling to use system package
+        version="2.5.2",  # Fixed RPM numpy dependency for pyqtgraph
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
