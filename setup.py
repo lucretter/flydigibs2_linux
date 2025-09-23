470222baf6ce7512ca1dfb089fe40f1174f1cbfd
Lucretter <aboukhait.omar@gmail.com>
2025-09-24 00:28:34 +0200

    Fix RPM PyQt6 compatibility by using system package
    - Remove bundled PyQt6 from RPM packages to avoid Qt6 version conflicts
    - RPM packages now use system python3-pyqt6 instead of bundled PyQt6
    - .deb packages continue to bundle PyQt6 for compatibility
    
    Fixes ImportError: version 'Qt_6_PRIVATE_API' not found

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bs2pro-controller",
-    version="2.5.1",  # Fixed RPM package PyQt6 dependency name
+    version="2.5.2",  # Fixed RPM PyQt6 bundling to use system package
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
