6ddb1bb401a9220530affa0a5e28f9508d573904
Lucretter <aboukhait.omar@gmail.com>
2025-09-24 00:05:29 +0200

    Bump version to 2.4.5 for pyqtgraph dependency fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bs2pro-controller",
-    version="2.4.4",  # Fixed HID device opening API compatibility
+    version="2.4.5",  # Added missing pyqtgraph dependency
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
